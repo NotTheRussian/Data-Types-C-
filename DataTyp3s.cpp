ae49c2bda0a0367bcd875ad408d59b0939e4bcc1
Jerry <111554668+NotTheRussian@users.noreply.github.com>
2022-11-14 19:38:05 -0600

    Add files via upload
    The basic Data types of c++.

---
+++ DataTyp3s.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+     char letter = 'A' ;
+    string simpletext = "I need your money." ;
+    int Whole = 25;
+    double  Decimal = 2.5;
+    cout << "This are examples of basic data types, try to change them!" << endl;
+    cout <<  simpletext << endl;
+    cout << letter << endl;
+    cout << Whole << endl;
+    cout << Decimal << endl;
+    return 0;
+}
